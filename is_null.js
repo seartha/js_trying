808f8c85be8a30ca10f16000fbe9b16e4c3541c7
seartha <62803886+seartha@users.noreply.github.com>
2020-07-10 01:13:01 +0300

    Update is_null.js

--- is_null.js
+++ is_null.js
@@ -1,6 +1,15 @@
-var assert = require('Chai').assert;
-var s =1;
-test('#isNull', function(){
-    assert.isNull(null, 'this is an optional error description - e.g. null is null');
-   
-    });
+var chai = require('chai');
+var assert = chai.assert;
+
+suite('Unit Tests', function(){
+  
+  // Make ALL tests pass
+  // !! Don't scramble the Assertions. We rely on their order to check the results !!
+  suite('Basic Assertions', function() {
+    /** assert.fail() will always fail. Change it into something more useful... **/
+  
+    /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
+    test('#isNull, #isNotNull', function(){
+      assert.isNull(null, 'this is an optional error description - e.g. null is null');
+      assert.isNotNull( 1, '1 is not null');
+    });}
