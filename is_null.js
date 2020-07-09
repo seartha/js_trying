886289049d880529b1eb7ef5928df3784b79fe02
seartha <62803886+seartha@users.noreply.github.com>
2020-07-10 01:08:29 +0300

    Update is_null.js

--- is_null.js
+++ is_null.js
@@ -1,12 +1,6 @@
-var assert = require('assert');
-var someArray = [1,2,3,4,5], 
-	len = someArray.length, 
-	i = 0; 
- 
-var count = 0; 
- 
-for ( ; i < len; i++ ) (function(i) { 
-	setTimeout(function() { 
-		assert( count++ === i, 'Checking the value of: ' + i ); 
-	}, i * 300); 
-})(i);
+var assert = require('Chai').assert;
+var s =1;
+test('#isNull', function(){
+    assert.isNull(null, 'this is an optional error description - e.g. null is null');
+   
+    });
